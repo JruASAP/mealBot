9277f36ea6d54d4177695a6503a67fc0b85202ca
Drew Sapolnick <jruasap@gmail.com>
2024-04-07 15:36:58 -0400

    updated format to arrow functions/logic refactor

--- mealBot.js
+++ mealBot.js
@@ -3,129 +3,104 @@ const dotenv = require("dotenv");
 const axios = require('axios');
 const chalk = require('chalk');
 const nodemailer = require("nodemailer");
-dotenv.config(); 
+const moment = require("moment");
+dotenv.config();
 
 
-async function getMeals(){
-    try {
-        const gs = new Sheets(process.env.GOOGLE_SHEET_ID);
-        await gs.authorizeApiKey(process.env.GOOGLE_SHEET_KEY);
-        const Sunday = await gs.tables("Sunday");
-        const Monday = await gs.tables("Monday");
-        const Tuesday = await gs.tables("Tuesday");
-        const Wednesday = await gs.tables("Wednesday");
-        const Thursday = await gs.tables("Thursday");
-        const Friday = await gs.tables("Friday");
-  /*      console.log(Sundaytable.headers);
-        console.log(Sundaytable.rows);*/
-        return [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday]
-        } catch (err) {    console.error(err);  }
+const getMeals = async()=> {
+  try {
+    const gs = new Sheets(process.env.GOOGLE_SHEET_ID);
+    await gs.authorizeApiKey(process.env.GOOGLE_SHEET_KEY);
+    const Sunday = await gs.tables("Sunday");
+    const Monday = await gs.tables("Monday");
+    const Tuesday = await gs.tables("Tuesday");
+    const Wednesday = await gs.tables("Wednesday");
+    const Thursday = await gs.tables("Thursday");
+    const Friday = await gs.tables("Friday");
+    return [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday]
+  } catch (err) { console.error(err); }
 }
 
-async function checkWeather(){
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${process.env.lat}&lon=${process.env.lon}&exclude=current,hourly,minutely&APPID=${process.env.WEATHER_KEY}&units=imperial`;
-   // console.log(url) 
-    const weather = await axios.get(url)
-return weather.data
+const checkWeather = async()=> {
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${process.env.lat}&lon=${process.env.lon}&exclude=current,hourly,minutely&APPID=${process.env.WEATHER_KEY}&units=imperial`;
+  const weather = await axios.get(url)
+  return weather.data
 }
 
-async function mailMeals(sundaymeal,sundayweather) {
- 
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true, // true for 465, false for other ports
-      auth: {
-        user: process.env.emailUser, 
-        pass: process.env.emailAppPass, 
-      },
-    });
-  
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: `"Meal Bot" <${process.env.emailUser}>`,
-      to: `${process.env.emailRecipients}`, 
-      subject: "Meal Bot Generated Meals 🤖", 
-      text: `Congratulations, your meal for Sunday is ${sundaymeal.Meal.value}\n Please review the ingredients needed-\n ${sundaymeal.Ingredients.value}\n If you would prefer to grill the weather will be ${sundayweather}`, 
-//      html: "<b>Hello world?</b>", 
-    });
-  
-    console.log("Message sent: %s", info.messageId);
+const mailMeals = async(message)=> {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.emailUser,
+      pass: process.env.emailAppPass,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: `"Meal Bot" <${process.env.emailUser}>`,
+    to: process.env.emailRecipients,
+    subject: "Meal Bot Generated Meals 🤖",
+    text: message,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+}
+
+const discordPost = async(mealMessage)=> {
+  const discordHook = process.env.discordHook;
+  const message = {
+    username: "Meal Bot",
+    avatar_url: "https://files.oaiusercontent.com/file-gzrBICmMw1H6vn8i0DxohudW?se=2024-04-07T00%3A21%3A36Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D9ed19cf9-0d38-4d29-9450-27a0a63b1d1c.webp&sig=BtKYMOHPOZvH3nLyauaoP0pN3Ckny8kiy7PcrIuW5KA%3D",
+    content: mealMessage,
   };
 
-  async function discordPost(mealMessage) {
-    const discordHook = process.env.discordHook; 
-    const message = {
-      username: "Meal Bot",
-      content: mealMessage, // The message content
-    };
-  
-    try {
-      await axios.post(discordHook, message);
-      console.log("Message posted to Discord successfully!");
-    } catch (error) {
-      console.error("Error posting message to Discord:", error);
-    }
+  try {
+    await axios.post(discordHook, message);
+    console.log("Message posted to Discord successfully!");
+  } catch (error) {
+    console.error("Error posting message to Discord:", error);
   }
-  
-  
-
-async function init(){
-    const meals = await getMeals();
-   // console.log(meals[0].rows);
-   const weather = await checkWeather();
-   // let forecast = weather.weather[0].description;
-    //console.log(weather);
-    let randomMealNumSun = Math.floor(Math.random() * meals[0].rows.length);
-    let randomMealNumMon = Math.floor(Math.random() * meals[1].rows.length);
-    let randomMealNumTues = Math.floor(Math.random() * meals[2].rows.length);
-    let randomMealNumWed = Math.floor(Math.random() * meals[3].rows.length);
-    let randomMealNumThurs = Math.floor(Math.random() * meals[4].rows.length);
-    let randomMealNumFri = Math.floor(Math.random() * meals[5].rows.length);
-    let randomMealRecipeSun = meals[0].rows[randomMealNumSun];
-    let randomMealRecipeMon = meals[1].rows[randomMealNumMon];
-    let randomMealRecipeTues = meals[2].rows[randomMealNumTues];
-    let randomMealRecipeWed = meals[3].rows[randomMealNumWed];
-    let randomMealRecipeThurs = meals[4].rows[randomMealNumThurs];
-    let randomMealRecipeFri = meals[5].rows[randomMealNumFri];
-    console.log(chalk.blue(`Congratulations, your meal for Sunday is ${randomMealRecipeSun.Meal.value} please review the ingredients needed- ${randomMealRecipeSun.Ingredients.value}`));
-    console.log(chalk.magenta(`Congratulations, your meal for Monday is ${randomMealRecipeMon.Meal.value} please review the ingredients needed- ${randomMealRecipeMon.Ingredients.value}`));
-    console.log(chalk.green(`Congratulations, your meal for Tuesday is ${randomMealRecipeTues.Meal.value} please review the ingredients needed- ${randomMealRecipeTues.Ingredients.value}`));
-    console.log(chalk.yellow(`Congratulations, your meal for Wednesday is ${randomMealRecipeWed.Meal.value} please review the ingredients needed- ${randomMealRecipeWed.Ingredients.value}`));
-    console.log(chalk.whiteBright(`Congratulations, your meal for Thursday is ${randomMealRecipeThurs.Meal.value} please review the ingredients needed- ${randomMealRecipeThurs.Ingredients.value}`));
-    console.log(chalk.red(`Congratulations, your meal for Friday is ${randomMealRecipeFri.Meal.value} please review the ingredients needed- ${randomMealRecipeFri.Ingredients.value}`));
-    try{
-    const mailed = await mailMeals(randomMealRecipeSun,weather.daily[2].weather[0].description);
-   }catch(err){
-    console.log(chalk.red(err));
-   }
-   try{
-    let message = `**This week's meals are:**
-
-    **Sunday:** Congratulations, your meal for Sunday is ${randomMealRecipeSun.Meal.value}. 
-      Please review the ingredients needed: ${randomMealRecipeSun.Ingredients.value}
-    
-    **Monday:** Congratulations, your meal for Monday is ${randomMealRecipeMon.Meal.value}. 
-      Please review the ingredients needed: ${randomMealRecipeMon.Ingredients.value}
-    
-    **Tuesday:** Congratulations, your meal for Tuesday is ${randomMealRecipeTues.Meal.value}. 
-      Please review the ingredients needed: ${randomMealRecipeTues.Ingredients.value}
-    
-    **Wednesday:** Congratulations, your meal for Wednesday is ${randomMealRecipeWed.Meal.value}. 
-      Please review the ingredients needed: ${randomMealRecipeWed.Ingredients.value}
-    
-    **Thursday:** Congratulations, your meal for Thursday is ${randomMealRecipeThurs.Meal.value}. 
-      Please review the ingredients needed: ${randomMealRecipeThurs.Ingredients.value}
-    
-    **Friday:** Congratulations, your meal for Friday is ${randomMealRecipeFri.Meal.value}. 
-      Please review the ingredients needed: ${randomMealRecipeFri.Ingredients.value}
-    `; 
-    await discordPost(message);
-  }catch(err){
-    console.log(chalk.red(err))
-  }
-
 }
+
+
+
+const init = async()=> {
+  const meals = await getMeals();
+  const weather = await checkWeather();
+
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  const chalkColors = [chalk.blue, chalk.magenta, chalk.green, chalk.yellow, chalk.whiteBright, chalk.red];
+  let discordAndEmailMessage = '**This week\'s meals and weather forecast:**\n';
+
+  meals.forEach((dayMeals, index) => {
+    if (index < weather.daily.length) {
+      const randomMealNum = Math.floor(Math.random() * dayMeals.rows.length);
+      const randomMealRecipe = dayMeals.rows[randomMealNum];
+      const dayForecast = weather.daily[index];
+      const formattedDate = moment.unix(dayForecast.dt).format('dddd MMMM DD, YYYY');
+
+      console.log(chalkColors[index % chalkColors.length](`Congratulations, your meal for ${formattedDate} is ${randomMealRecipe.Meal.value}. Please review the ingredients needed- ${randomMealRecipe.Ingredients.value} Weather Forecast: ${dayForecast.weather[0].description}, High: ${dayForecast.temp.max}°, Low: ${dayForecast.temp.min}°`));
+
+      discordAndEmailMessage += `
+**${formattedDate}:** Congratulations, your meal is ${randomMealRecipe.Meal.value}. Please review the ingredients needed: ${randomMealRecipe.Ingredients.value}
+Weather Forecast: ${dayForecast.weather[0].description}, High: ${dayForecast.temp.max}°, Low: ${dayForecast.temp.min}°
+`;
+    }
+  });
+
+  try {
+    await discordPost(discordAndEmailMessage);
+  } catch (err) {
+    console.log(chalk.red(err));
+  };
+
+  try {
+    await mailMeals(discordAndEmailMessage);
+  } catch (err) {
+    console.log(chalk.red('Error sending email:'), err);
+  };
+};
 
 init();
